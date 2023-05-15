9a2e6ab4469ece04697fffef37046e1fa600b572
thiswind <23764+thiswind@users.noreply.github.com>
2023-05-15 09:41:34 +0800

    Update netfetch.dart

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
   final httpPackageUrl = Uri.https('raw.githubusercontent.com',
-      '/YN3011140075/my_-netfetch/main/my_data.json');
+      '/YN3011140075/my_netfetch/main/my_data.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
 }
